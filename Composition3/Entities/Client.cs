f266abf40c83724da5e641409a33b0ef9d47ba16
arthurvenicio <stuks.midia@gmail.com>
2022-02-08 00:13:38 -0300

    Add project files.

---
+++ Composition3/Entities/Client.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Composition3.Entities
+{
+    internal class Client
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        
+        public DateTime BirthDate { get; set; }
+
+        public Client()
+        {
+        }
+
+        public Client(string name, string email, DateTime birthDate)
+        {
+            this.Name = name;
+            this.Email = email;
+            this.BirthDate = birthDate;
+        }
+
+        public override string ToString()
+        {
+            return Name
+                + ", ("
+                + BirthDate.ToString("dd/MM/yyyy")
+                + ") - "
+                + Email;
+        }
+
+
+    }
+}
---
+++ Composition3/Entities/Order.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+using Composition3.Entities;
+using Composition3.Entities.Enums;
+
+namespace Composition3.Entities
+{
+    internal class Order
+    {
+        public DateTime Moment { get; set; }
+        public OrderStatus Status { get; set; }
+        public Client Client { get; set; }
+        public List<OrderItem> Items { get; set; } = new List<OrderItem>();
+
+        public Order()
+        {
+        }
+
+        public Order(DateTime moment, OrderStatus status, Client client)
+        {
+            this.Moment = moment;
+            this.Status = status;
+            this.Client = client;
+        }
+
+        public void AddItem(OrderItem order)
+        {
+            Items.Add(order);
+        }
+
+        public void RemoveItem(OrderItem order)
+        {
+            Items.Remove(order);
+        }
+
+
+        public double Total()
+        {
+            double total = 0;
+
+            foreach (OrderItem item in Items)
+            {
+                total += item.SubTotal();
+            }
+
+            return total;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("Order moment: " + Moment.ToString("dd/MM/yyyy HH:mm:ss"));
+            sb.AppendLine("Order status: " + Status);
+            sb.AppendLine("Client: " + Client);
+            sb.AppendLine("Order items:");
+            foreach (OrderItem item in Items)
+            {
+                sb.AppendLine(item.ToString());
+            }
+            sb.AppendLine("Total price: $" + Total().ToString("F2", CultureInfo.InvariantCulture));
+            return sb.ToString();
+        }
+
+    }
+}
